164c6f1ec977aaf347efb98d99249cda15d9ed4e
Krishna4563 <daskrishna0902@gmail.com>
2024-06-03 14:11:23 +0530

    server update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectDB } = require("./config/db");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
@@ -20,6 +21,16 @@ app.use(express.json());
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/lists", require("./routes/listRoutes"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
